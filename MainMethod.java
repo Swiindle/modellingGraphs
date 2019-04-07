9673a3c601b10a765514ddd07ff0437a7d2cb2be
Mark <18markl@dyn-v-10-34-199-118.resnet.local>
2019-04-07 15:23:36 +0100

    Adding the foundations of the project

---
+++ MainMethod.java
@@ -0,0 +1,12 @@
+/**
+ * Run this class to open the 'game'
+ *
+ */
+public class MainMethod
+{
+    public static void main(String args[])
+    {
+        VisualControl vc = new VisualControl();
+        vc.open();
+    }
+}
---
+++ VisualControl.java
@@ -0,0 +1,16 @@
+public class VisualControl
+{
+    private GameArena ga = new GameArena(600,600);
+    public VisualControl()
+    {
+    }
+    public void open()
+    {
+        System.out.printf("open\n");
+    }
+    public void createGraph1()
+    {
+        Ball b = new Ball();
+        Line l = new Line();
+    }
+}
